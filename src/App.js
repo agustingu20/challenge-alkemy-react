a9b933b3eb8962cefe9ad4c1d95962719a3c0a5c
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2021-08-21 16:03:38 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+function App() {
+  <Router>
+    <Switch>
+
+    </Switch>
+  </Router>
+}
+
+export default App;
