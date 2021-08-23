71f7d02a8649c8fffc6bf8230dd03bb97fed5161
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2021-08-22 21:09:03 -0300

    peticion a superhero api, props a componente superheroescard y modal ver detalles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
 import SuperheroesCard from "./SuperheroesCard";
+import axios from "axios";
+import { Form } from "react-bootstrap";
 
 export default function Home() {
-  return <SuperheroesCard />;
+  const [heroes, setHeroes] = useState({});
+  const [heroName, setHeroName] = useState("70");
+
+  useEffect(() => {
+    getHero();
+  });
+
+  const getHero = async () => {
+    const response = await axios.get(
+      `https://superheroapi.com/api/10224034783111933/70`
+    );
+    setHeroes(response.data);
+  };
+
+  return (
+    <div>
+      <SuperheroesCard
+        name={heroes.name}
+        imageUrl={heroes.image?.url}
+        intelligence={heroes.powerstats?.intelligence}
+        strength={heroes.powerstats?.strength}
+        speed={heroes.powerstats?.speed}
+        durability={heroes.powerstats?.durability}
+        power={heroes.powerstats?.power}
+        combat={heroes.powerstats?.combat}
+        hero={heroes}
+      />
+    </div>
+  );
 }
--- src/components/SuperheroesCard.jsx
+++ src/components/SuperheroesCard.jsx
@@ -1,17 +1,57 @@
-import { Button, Card } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Card, Modal } from "react-bootstrap";
 
-export default function SuperheroesCard() {
+export default function SuperheroesCard(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={props.imageUrl} />
+        <Card.Body>
+          <Card.Title>{props.name}</Card.Title>
+          <div>
+            <ul>
+              <li>Inteligencia: {props.intelligence}</li>
+              <li>Agilidad: {props.strength}</li>
+              <li>Velocidad: {props.speed}</li>
+              <li>Durabilidad: {props.durability}</li>
+              <li>Poder: {props.power}</li>
+              <li>Combate: {props.combat}</li>
+            </ul>
+          </div>
+          <Button variant="primary" onClick={handleShow}>
+            Ver detalles
+          </Button>
+        </Card.Body>
+      </Card>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Detalles</Modal.Title>
+        </Modal.Header>
+        <div>
+          <ul>
+            <li>Peso: {props.hero.appearance?.weight.slice(1)} </li>
+            <li>Altura: {props.hero.appearance?.height.slice(1)} </li>
+            <li>Nombre: {}</li>
+            <li>Alias: {props.hero.biography?.aliases.slice(1)}</li>
+            <li>Color de ojos: {}</li>
+            <li>Color de cabello: {}</li>
+            <li>Lugar de trabajo: {props.hero.work?.base}</li>
+          </ul>
+        </div>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 }
